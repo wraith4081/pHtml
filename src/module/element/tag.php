db2a3377a0d950e1498553afb6cf72678cc50c8b
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2021-03-26 13:52:50 +0300

    Update tag.php

--- src/module/element/tag.php
+++ src/module/element/tag.php
@@ -8,8 +8,16 @@ class tag {
   function createvalue($tag, $text, $type) {
     var returnvalue = "<" . $tag . ">" . $text . "</" . $tag . ">";
     $type $returnvalue;
+    var tagcount
+    if ($tagcount) {
+      $tagcount = $tagcount + 1;
+    } else {
+      $tagcount = 1;
+     }
   }
 }
-
+    function count() {
+      return $tagcount;
+     }
 $tag = new tag();
 ?>
