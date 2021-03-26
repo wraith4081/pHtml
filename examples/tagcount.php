3159f5df4f72347bcdfd9711e5fb7f0c7d0e6c34
wraith4081 <54374743+wraith4081@users.noreply.github.com>
2021-03-26 13:54:37 +0300

    Create tagcount.php

---
+++ examples/tagcount.php
@@ -0,0 +1,15 @@
+  
+<?php 
+// Get a function library
+
+  include "module/element/tag.php";
+
+
+// Basic using $tag->count()
+// this function help to counts a created pHtml tags
+
+  echo $tag->count();
+
+
+
+?>
